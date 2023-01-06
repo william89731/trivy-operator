95173ff5ef6ce40fb374ad2f9f612bffc6dc95bb
william89731 <willystile@gmail.com>
2023-01-06 03:44:04 +0100

    commit script

--- operator.sh
+++ operator.sh
@@ -31,15 +31,21 @@ function info { echo -e "\e[32m[info] $*\e[39m"; }
 function warn  { echo -e "\e[33m[warn] $*\e[39m"; }
 function error { echo -e "\e[31m[error] $*\e[39m"; exit 1; }
 
-info "read namespace list"
+info "create report"
 truncate -s 0 ~/trivy-operator/report.txt
 set -o noclobber
 for NAMESPACE in $(cat $1); do
-  echo ""
-  $NAMESPACE >>  ~/trivy-operator/report.txt
+  echo "[NAMESPACE $NAMESPACE]" >>  ~/trivy-operator/report.txt
+  echo "**vuln**"  >>  ~/trivy-operator/report.txt
   kubectl get vuln -o wide -n $NAMESPACE >>  ~/trivy-operator/report.txt
+  echo " " >>  ~/trivy-operator/report.txt
+  echo "**configaudit**" >>  ~/trivy-operator/report.txt
   kubectl get configaudit -o wide -n $NAMESPACE >>  ~/trivy-operator/report.txt
-  echo ""
+  echo " " >>  ~/trivy-operator/report.txt
+  echo " " >>  ~/trivy-operator/report.txt
+  echo "-----------------------------------------------------------------------------------------" >>  ~/trivy-operator/report.txt
 done
+sleep 3
+clear
 info "see result in report.txt"
 date  >>  ~/trivy-operator/report.txt
